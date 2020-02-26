5b0b59ae81e2b77f133beba2403b0bb495fd7b87
Anthony Lavender <Alavender199@gmail.com>
2020-02-26 17:14:08 +0000

    Started email sending for contact form

--- Website/index.js
+++ Website/index.js
@@ -1,13 +1,36 @@
 'use strict';
 
 function sentButtonClick(){
-    let  popup = document.getElementById("sendPopUp");
-    popup.style.visibility = "visible";
+    let nameInputEle = document.getElementById("nameInput");
+    let emailInputEle = document.getElementById("emailInput");
+    let messageInputEle = document.getElementById("subjectInput");
+
+    let nameText = nameInputEle.value;
+    let emailText = emailInputEle.value;
+    let messageInput = messageInputEle.value;
+    
+//    nameInputEle.value = "";
+//    emailInputEle.value = "";
+//    messageInputEle.value = "";
+    
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    let sel = document.getSelection();
+    sel.removeAllRanges();
 }
 
-
+function resetButtonClick(){
+    let nameInputEle = document.getElementById("nameInput");
+    let emailInputEle = document.getElementById("emailInput");
+    let messageInputEle = document.getElementById("subjectInput");
+    
+    nameInputEle.value = "";
+    emailInputEle.value = "";
+    messageInputEle.value = "";
+}
 
 window.onload = function () {
-    document.getElementById("sendPopUp").addEventListener("click", sentButtonClick)
+    document.getElementById("send_button").addEventListener("click", sentButtonClick);
+    document.getElementById("reset_button").addEventListener("click", resetButtonClick);
     
 }
