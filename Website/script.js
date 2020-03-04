b315d80389c1ab080c0ea7d31f0e52ba759ae29a
AdamRichards3 <adamwilliamrichards@gmail.com>
2020-03-04 22:16:35 +0000

    Changed the colour on heading
    Maked sure the titles where the right colour

--- Website/script.js
+++ Website/script.js
@@ -2,3 +2,5 @@ $('.menu-burger, .menu-items').on('click', function() {
   $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
   $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
 });
+
+//☰ ✕ |  //
