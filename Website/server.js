4190dab1d7f9de69e49cf9d9fc9a09e16a0ebcdd
AdamRichards3 <adamwilliamrichards@gmail.com>
2020-03-11 11:41:46 +0000

    Create server.js
    Created server and added database

---
+++ Website/server.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+
+app.use(express.static(__dirname));
+app.use(express.static("public"))
+app.use(express.json());
+
+app.listen(8080, (err) => {
+  if (err) console.error('error starting server', err);
+  else console.log('server started');
+});
+
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "myusername",
+    password: "mypassword"
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  
+
+function error(res, msg) {
+  res.sendStatus(500);
+  console.error(msg);
+}
