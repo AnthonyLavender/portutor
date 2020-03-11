3561cc3318b94d61da2dd3efa1ee0b1b088c01ab
AdamRichards3 <adamwilliamrichards@gmail.com>
2020-03-11 12:19:04 +0000

    Added server
    added packages to create a server

--- Website/server.js
+++ Website/server.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const express = require('express');
+// const express = require('express');
 const mysql = require('mysql');
 
-const app = express();
+// const app = express();
 
 
-app.use(express.static(__dirname));
-app.use(express.static("public"))
-app.use(express.json());
+// app.use(express.static(__dirname));
+// app.use(express.static("public"))
+// app.use(express.json());
 
-app.listen(8080, (err) => {
-  if (err) console.error('error starting server', err);
-  else console.log('server started');
-});
+// app.listen(8080, (err) => {
+// if (err) console.error('error starting server', err);
+// else console.log('server started');
+// });
 
 
 const con = mysql.createConnection({
@@ -28,7 +28,7 @@ const con = mysql.createConnection({
   });
   
 
-function error(res, msg) {
-  res.sendStatus(500);
-  console.error(msg);
-}
+// function error(res, msg) {
+//   res.sendStatus(500);
+//   console.error(msg);
+// }
