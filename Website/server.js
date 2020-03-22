62bb981680053062d9f0088af12fc15a40247f97
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2020-03-22 20:10:08 +0000

    Npm start working
    fixed json file and now when doing npm start the website will run

--- Website/server.js
+++ Website/server.js
@@ -15,17 +15,17 @@ else console.log('server started');
 });
 
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "myusername",
-    password: "mypassword"
-  });
-
-  con.connect(function(err) {
-    if (err) console.log(err);
-    console.log("Connected!");
-  });
-
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     user: "myusername",
+//     password: "mypassword"
+//   });
+//
+//   con.connect(function(err) {
+//     if (err) console.log(err);
+//     console.log("Connected!");
+//   });
+//
 
 function error(res, msg) {
   res.sendStatus(500);
@@ -36,26 +36,26 @@ function hash(){
   ///when has function gets made///
 }
 
-function insertProfile(res,msg,fullName,email,password,username,file,desc,location,price,avail,tutor,subject,stars){
-    app.get('/addProfile', (req,res) => {
-        let profile = {
-          profileName:fullName,
-          profileEmail:email,
-          profilePassword:hash(password),
-          profileUsername:username,
-          profilePrictureFile:file,
-          profileBio:desc,
-          profileTutorLocation:location,
-          profileTutorPrice:price,
-          profileAvailabilty:avail,
-          profileTutor:tutor,
-          profileSubjects:subject,
-          profileStars:stars,}
-        let sql = 'INSERT INTO profile SET ?';
-        let query = db.query(sql, profile, (err, result) => {
-          if(err) throw err;
-          console.log(result);
-          res.send('ProfileAdded...')
-        })
-    })
-}
+// function insertProfile(res,msg,fullName,email,password,username,file,desc,location,price,avail,tutor,subject,stars){
+//     app.get('/addProfile', (req,res) => {
+//         let profile = {
+//           profileName:fullName,
+//           profileEmail:email,
+//           profilePassword:hash(password),
+//           profileUsername:username,
+//           profilePrictureFile:file,
+//           profileBio:desc,
+//           profileTutorLocation:location,
+//           profileTutorPrice:price,
+//           profileAvailabilty:avail,
+//           profileTutor:tutor,
+//           profileSubjects:subject,
+//           profileStars:stars,}
+//         let sql = 'INSERT INTO profile SET ?';
+//         let query = db.query(sql, profile, (err, result) => {
+//           if(err) throw err;
+//           console.log(result);
+//           res.send('ProfileAdded...')
+//         })
+//     })
+// }
