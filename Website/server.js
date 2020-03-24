fedf6b49cc3595edd12f7726749467ee5b92e9f7
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2020-03-24 21:30:30 +0000

    Changed to local database
    Changed from mysql to nedb.

--- Website/server.js
+++ Website/server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const mysql = require('mysql');
+const DataStore = require('nedb');
 const app = express();
 
 
@@ -14,54 +14,13 @@ if (err) console.error('error starting server', err);
 else console.log('server started');
 });
 
-
-const con = mysql.createConnection({
-    host: "127.0.0.1",
-    port: "8081",
-    user: "Admin",
-    password: "password123",
-    database: "SoftEngDatabase" 
-  });
-
-  con.connect(function(err) {
-    if (err) console.log(err);
-    console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function(err,result){
-      if(err) throw err;
-         console.log(result);
-    })
-  });
-
-
 function error(res, msg) {
   res.sendStatus(500);
   console.error(msg);
 }
 
-function hash(){
-  ///when has function gets made///
-}
-
-// function insertProfile(res,msg,fullName,email,password,username,file,desc,location,price,avail,tutor,subject,stars){
-//     app.get('/addProfile', (req,res) => {
-//         let profile = {
-//           profileName:fullName,
-//           profileEmail:email,
-//           profilePassword:hash(password),
-//           profileUsername:username,
-//           profilePrictureFile:file,
-//           profileBio:desc,
-//           profileTutorLocation:location,
-//           profileTutorPrice:price,
-//           profileAvailabilty:avail,
-//           profileTutor:tutor,
-//           profileSubjects:subject,
-//           profileStars:stars,}
-//         let sql = 'INSERT INTO profile SET ?';
-//         let query = db.query(sql, profile, (err, result) => {
-//           if(err) throw err;
-//           console.log(result);
-//           res.send('ProfileAdded...')
-//         })
-//     })
-// }
+profileDb.loadDatabase();
+bookingDb.loadDatabase();
+transactionDb.loadDatabase();
+profileBookingDb.loadDatabase();
+reviewsDb.loadDatabase();
