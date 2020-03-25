97dad10cb96220a843e8f15e7f8a5150f88825bb
Mohammed Atoum <moatoum@gmail.com>
2020-03-25 21:55:10 +0200

    Slight CSS/HTML updates + Fix burger menu not working on tutor page

--- Website/script.js
+++ Website/script.js
@@ -2,5 +2,3 @@ $('.menu-burger, .menu-items').on('click', function() {
   $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
   $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
 });
-
-//☰ ✕ |  //
