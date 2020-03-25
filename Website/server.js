d90a6878b387d1755c52671a5c047e15290e2490
Tino <45016828+ionfr@users.noreply.github.com>
2020-03-25 18:51:34 +0000

    organised scripts

--- Website/server.js
+++ Website/server.js
@@ -10,8 +10,8 @@ app.use(express.static("public"))
 app.use(express.json());
 
 app.listen(8080, (err) => {
-if (err) console.error('error starting server', err);
-else console.log('server started');
+  if (err) console.error('error starting server', err);
+  else console.log('server started');
 });
 
 function error(res, msg) {
