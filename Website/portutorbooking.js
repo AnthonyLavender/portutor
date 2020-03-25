da8593ec255ff8e4a5ead0d06e84ad92bb86c573
Mohammed Atoum <moatoum@gmail.com>
2020-03-25 21:26:35 +0200

    Added the booking calendar to tutor profile
    A booking calendar with an option to request to book a slot. The user can also move an existing booked session around the calendar's other available slots if they wish to change their booking.

---
+++ Website/portutorbooking.js
@@ -0,0 +1,28 @@
+var p = PorTutor.Scheduling;
+var calendar = new p.Calendar(document.getElementById("calendar"));
+
+calendar.currentView = p.CalendarView.SingleMonth;
+
+calendar.theme = "light";
+calendar.itemSettings.titleFormat = "%s[hh:mm tt] %h";
+
+calendar.render();
+
+calendar.useForms = false;
+calendar.itemDoubleClick.addEventListener(handleItemDoubleClick);
+calendar.selectionEnd.addEventListener(handleSelectionEnd);
+
+
+function handleItemDoubleClick(sender, args) {
+	var form = new TimeForm(sender, args.item, "edit");
+	form.showForm();
+}
+
+function handleSelectionEnd(sender, args) {
+	var item = new p.Item();
+	item.startTime = args.startTime;
+	item.endTime = args.endTime;
+
+	var form = new TimeForm(sender, item, "new");
+	form.showForm();
+}
