ef23760229e29d989a5fdcf7c683a693f4127c9c
Anthony Lavender <Alavender199@gmail.com>
2020-03-25 17:14:26 +0000

    Node modules

--- Website/database/databaseInstructions.js
+++ Website/database/databaseInstructions.js
@@ -11,3 +11,28 @@ bookingDb.loadDatabase();
 transactionDb.loadDatabase();
 profileBookingDb.loadDatabase();
 reviewsDb.loadDatabase();
+
+function idCreate(db){
+    id=Math.random().toString(36).substr(2, 9);
+    if (!!idCheck(id)){idCreate(db);}
+    return id
+}
+
+
+function insertProfile(name,email,password,UserName,pictureFileName,desc,
+  location,price,tutor,subject,stars){
+   profileDb.insert({
+     profile_ID: idCreate(profileDb),
+     profile_Name: name,
+     profile_Email:email,
+     profile_Password:passwordHash.generate(password),
+     profile_Username:userName,
+     profile_Picture_File:pictureFileName,
+     profile_Bio:desc,
+     profile_Tutor_Location:location,
+     profile_Tutor_Price:price,
+     profile_Tutor:tutor,
+     profile_Subject:subject,
+     profile_Stars:stars
+   });
+};
