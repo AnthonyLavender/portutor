5edc4ef094999474ca4cca815dd9b650dd279649
JamesKempadoo <44989874+JamesKempadoo@users.noreply.github.com>
2020-03-27 12:23:46 +0000

    Send to account pages

--- Website/tutorSearch.js
+++ Website/tutorSearch.js
@@ -4,38 +4,45 @@ async function getData(){
     console.log("test");
     const response = await fetch('/getProfileData');
     const data = await response.json();
-    tutorSeperator(data);
+    const object = document.getElementsByClassName("btn");
+    object.onclick = () => tutorSeperator(data);
     
 }
 
+ function tutorSeperator(data){
+     console.log(data[0]);
+     console.log("------------------------------------------");
+     console.log(data);
+     console.log("------------------------------------------");   
+}
 
-function tutorSeperator(data){
-    console.log(data[0]);
-    // const data=tutorSeperator(data);
-    console.log("------------------------------------------")
-    // console.log(tutorData);
-    // for (let i = 0; i<data.length-1;i++ ){
-    //     if (data[i].profile_Tutor==="False"){data.splice(i,1)};
-    // };  
-    console.log(data);
-    var btn = document.getElementsByClassName('btn');
-    let elementList = document.getElementsByClassName("form-input");
-    console.log("Hi");
-    let subjectValue = elementList[0].value;
-    // let degreeValue = elementList[1].value;
-    let LocationValue = elementList[2].value;
-    let RatingValue = elementList[3].value;
-    let PriceValue = elementList[4].value;
-
-    for (let i = 0; i<data.length-1;i++ ){
-        if (data[i].profile_Tutor==="False" || 
-        data[i].profile_Subject!=subjectValue ||
-        data[i].profile_Tutor_Location!=LocationValue 
+// const myForm = document.getElementsByClassName("tutorsearch-form");
+//     for (let j = 0; j<myForm.length-1;j++ ){
+//         console.log("boo")
+//         myForm[j].addEventListener("sumbit",()=>{
+//             e.preventDefault();
+//             const request = new XMLHttpRequest();
+//             request.open('post',"tutorresults.html")
+            
         
-        // data[i].profile_Stars!=RatingValue ||
-        // data[i].profile_profile_Tutor_Price!=PriceValue 
-        ){data.splice(i,1)};
-    };  
-    console.log(data);
-    
-}
+// const myForm = document.getElementsByClassName("tutorsearch-form");
+
+// var btn = document.getElementsByClassName('btn');
+//     let elementList = document.getElementsByClassName("form-input");
+//     console.log("Hi");
+//     let subjectValue = elementList[0].value;
+//     // let degreeValue = elementList[1].value;
+//     let LocationValue = elementList[2].value;
+//     let RatingValue = elementList[3].value;
+//     let PriceValue = elementList[4].value;
+
+//     for (let i = 0; i<data.length-1;i++ ){
+//         if (data[i].profile_Tutor==="False" || 
+//         data[i].profile_Subject!=subjectValue ||
+//         data[i].profile_Tutor_Location!=LocationValue 
+        
+//         // data[i].profile_Stars!=RatingValue ||
+//         // data[i].profile_profile_Tutor_Price!=PriceValue 
+//         ){data.splice(i,1)};
+//     };  
+//     console.log(data);
